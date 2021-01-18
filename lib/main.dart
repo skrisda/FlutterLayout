c87ff1bfc912cc2d8a573ccef8874b199435b499
Krisda Suwankarn <suwankarn@gmail.com>
2021-01-18 20:16:52 +0700

    completed

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,7 @@ import 'package:flutter_layout/row_widget_layout.dart';
 import 'package:flutter_layout/sizebox_widget_layout.dart';
 import 'package:flutter_layout/stack_widget_layout.dart';
 import 'package:flutter_layout/workshop_welcome_screen.dart';
-import 'center_widget_layout.dart';
+// import 'center_widget_layout.dart';
 
 void main() {
   runApp(MyApp());
